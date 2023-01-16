6f924212bffd61a04d7e5b047553e13dd2c58002
shubhxms <03.shubhamshah@gmail.com>
2023-01-16 12:41:17 +0530

    basic func done

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::env;
+use convert_case::{Case, Casing};
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+
+    let help = "
+    use format <type-to-convert-to> <text-to-convert>\n
+    following types are available
+    \n\tcapital,\n\tlower,\n\ttitle,\n\tsnake,\n\trandom\n
+    for text spanning multiple lines please use quotes
+    ";
+
+    if args.len() < 2 {
+        println!("not enough arguments supplied");
+        println!("{}", help);
+        std::process::exit(2);
+    }
+
+    let arg1 = &args[1];
+    let arg2 = &args[2];
+
+    
+
+    match arg1.as_str(){
+        "capital" => println!("{}", arg2.to_case(Case::Upper)),
+        "lower" => println!("{}", arg2.to_case(Case::Lower)),
+        "title" => println!("{}", arg2.to_case(Case::Title)),
+        "snake" => println!("{}", arg2.to_case(Case::Snake)),
+        "random" => println!("{}", arg2.to_case(Case::Random)),
+        "help" => println!("{}", help),
+        _ => println!("{}", help)
+    }
+
+
+}
